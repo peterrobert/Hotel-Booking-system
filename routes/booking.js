83816dccbc44a16358eb06bae52fcfbc96cd8ce7
Peter Robert <pwambui93@gmail.com>
2021-06-24 09:48:10 +0300

    Create the show route

--- routes/booking.js
+++ routes/booking.js
@@ -5,8 +5,8 @@ const Router = express.Router();
 const { Booking, bookingValidation } = require('../models/booking');
 const auth = require('../middleware/authorization')
 
-Router.get('/', auth, async (req, res) => {
-    const bookings = await Booking.find().populate('hotel', 'name location price')
+Router.get('/:hotelID', auth, async (req, res) => {
+    const bookings = await Booking.find({hotel: req.params.hotelID}).populate('hotel', 'name location price')
     if (bookings.length < 1) return res.status(200).send('There are no bookings at the moment');
     res.status(200).send(bookings)
 })
@@ -27,6 +27,12 @@ Router.post('/:hotelID', auth, (req, res) => {
     }).catch((err) => {
         res.send(err.details[0].message)
     })
+})
+
+Router.get('/:hotelID/:id', async (req, res) => {
+    const results = await Booking.findById(req.params.id);
+    if(!results) return res.status(404).send("There is no booking with that ID");
+    res.status(200).send(results);
 })
 
 module.exports = Router
